573e7931e8d965ad575e5d46d4e8417d67d539a2
Qusai Ahmad <q.ahmad@raca.edu.jo>
2025-07-22 03:27:48 +0300

    edited the width and styles of some pages

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/* --- THIS IS THE FIX --- */}
+      {/* 
+        Add the <head> tag here. Next.js will automatically combine 
+        these tags with the ones generated from your `metadata` object.
+      */}
+      <head>
+        {/* Preconnect to the most critical third-party domains to save time */}
+        <link rel="preconnect" href="https://your-perfect-cv.firebaseapp.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://apis.google.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://cdn.paddle.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://sandbox-cdn.paddle.com" crossOrigin="anonymous" />
+      </head>
+      {/* --- END OF FIX --- */}
+      
       <body className={inter.className}>
         <ClientLayout>
           {children}
